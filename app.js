07fefd9d1a8d48d6b67a71b38cc26fdf7f99d06e
kevinccbsg <kevinccbsg@gmail.com>
2018-02-27 00:04:38 +0100

    Configuración básica de express

---
+++ app.js
@@ -0,0 +1,28 @@
+const config = require('app-config');
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+
+const app = new express();
+
+app.use(compression());
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(expressSession({
+  secret: config.config.session.secret,
+  cookie: config.config.session.cookie,
+  resave: config.config.session.resave,
+  saveUninitialized: config.config.session.saveUninitialized,
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+module.exports = app;
