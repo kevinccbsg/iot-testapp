6b458b66202f3cf22b04e2cfecf087eb9aa4df71
kevinccbsg <kevinccbsg@gmail.com>
2018-03-06 00:05:15 +0100

    Añadido controlador para el oauth2

---
+++ controllers/oauthController.js
@@ -0,0 +1,59 @@
+const config = require('app-config');
+const clientHTTP = require('./../clientHTTP');
+
+const debug = require('debug')('IOT-APP:server');
+
+const client = clientHTTP(config.config.kongOptions);
+
+const oauthController = async (req, res) => {
+  const code = req.query.code;
+  const error = req.query.error;
+  const sess = req.session;
+  if (error) {
+    return res.redirect('/403');
+  }
+  try {
+    const { body } = await client.postRequest(config.config.tokenPath, {
+      client_id: config.config.client_id,
+      client_secret: config.config.client_secret,
+      code,
+      grant_type: 'authorization_code',
+    });
+    debug(body);
+    sess.access_token = body.access_token;
+    sess.refresh_token = body.refresh_token;
+    const accessToken = sess.access_token;
+    const tokenType = 'Bearer';
+    const contentType = 'application/json';
+    const authorization = `${tokenType} ${accessToken}`;
+    const headers = { 'Content-Type': contentType, Authorization: authorization };
+    const codeUser = '50006';
+    client.setHeaders(headers);
+    const apiResponse = await client.getRequest(`/userapi/user/${codeUser}`);
+    debug('Obtain user');
+    debug(apiResponse);
+    sess.user = apiResponse.body;
+    return res.redirect('/');
+  } catch (err) {
+    debug('[oauthController] Error');
+    debug(err);
+    if (err.status === 500) {
+      return res.redirect('/500');
+    }
+    return res.redirect('/403');
+  }
+};
+
+const ensureSession = (req, res, next) => {
+  debug('ensureSession');
+  const sess = req.session;
+  if (!sess.user) {
+    return res.redirect('/403');
+  }
+  return next();
+};
+
+module.exports = {
+  oauthController,
+  ensureSession,
+};
